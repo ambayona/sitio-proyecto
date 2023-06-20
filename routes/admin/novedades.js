65f175bed083363f833ea9b769cd2d1f579376b6
adriana bayona <ambayona9@gmail.com>
2023-06-20 09:57:31 -0300

    sitio proyecto

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+var novedades = await novedadesModel.getNovedades();
+
     res.render('admin/novedades', {
         layout: '/admin/layout',
-        persona: req.session.nombre
+        persona: req.session.nombre,
+        novedades
     });
 });
 
+//para eliminar una novedad//
+router.get('/eliminar/:id', async (req, res, next) => {
+    var id = req.params.id; //id2
+
+    await novedadesModel.deleteNovedadById(id);
+    res.redirect('/admin/novedades')
+}); //cierra get de eliminar//
 
 module.exports = router;
 
